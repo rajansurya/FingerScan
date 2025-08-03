d21d2c2edb5fbb4d0c8f9625f2773b32805c309f
rajansurya <rajansurya.88@gmail.com>
2025-08-03 11:47:17 +0530

    delte fi

--- app/src/main/java/com/example/touchlessfingerprintsdk/check/scan/TouchlessFingerprintSDK.kt
+++
@@ -1,8 +0,0 @@
-package com.example.touchlessfingerprintsdk.check.scan
-
-import android.app.Activity
-
-
-interface TouchlessFingerprintSDK {
-    suspend fun captureAndProcess(activity: Activity): FingerprintResult
-}
--- app/src/main/java/com/example/touchlessfingerprintsdk/starttouch/capture/TouchlessFingerprintSDK.kt
+++
@@ -1,8 +0,0 @@
-package com.example.touchlessfingerprintsdk.starttouch.capture
-
-import android.app.Activity
-import com.example.touchlessfingerprintsdk.starttouch.data.FingerprintResult
-
-interface TouchlessFingerprintSDK {
-    suspend fun captureAndProcess(activity: Activity): FingerprintResult
-}
